812d18a8d9c90ac1ced19da7b21b3af2b2d44aa5
Zofia Drozd <zegarynka@wp.pl>
2016-05-02 11:52:29 +0200

    add basic equipment modal

--- LittleHero/www/main/MainCtrl.js
+++ LittleHero/www/main/MainCtrl.js
@@ -1,6 +1,40 @@
-angular.module('littleHero').controller('MainController', function($scope, $state){
+angular.module('littleHero').controller('MainController', function($scope, $state, $ionicModal){
   $scope.swipeLeft = function() {
     console.log("swipe left");
     $state.go("tasks");
   };
-});
+
+  $scope.equipment = [
+    {
+      iconSrc: '',
+      imgSrc: 'img/eyes1.svg'
+    },
+    {
+      iconSrc: '',
+      imgSrc: 'img/hair1.svg'
+    },
+    {
+      iconSrc: '',
+      imgSrc: 'img/mouth1.svg'
+    },
+    {
+      iconSrc: '',
+      imgSrc: 'img/nose1.svg'
+    },
+    {
+      iconSrc: '',
+      imgSrc: 'img/pants1.svg'
+    }
+  ];
+
+  $ionicModal.fromTemplateUrl('equipmentModal.html', {
+    scope: $scope
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+
+  $scope.openModal = function() {
+    $scope.modal.show();
+  };
+
+  });
