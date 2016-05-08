1e92cf9baef539f772b7a3a946783c9aed1e9943
Anna Gabler <anka.gabler@gmail.com>
2016-05-08 22:54:30 +0200

    wrzucenie ionica do logwania

--- LittleHero/www/login/LoginCtrl.js
+++ LittleHero/www/login/LoginCtrl.js
@@ -1,4 +1,8 @@
 angular.module('littleHero').controller('LoginController', function($scope, $http){
 
-    
+  $scope.devList = [
+    { text: "Opiekun", checked: true},
+    { text: "Dziecko"},
+  ];
+111
 });
