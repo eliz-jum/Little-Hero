207abfef3f732fbc69a704421774d91f9597da40
Zofia Drozd <zegarynka@wp.pl>
2016-05-19 02:51:58 +0200

    add new assets

--- LittleHero/www/main/MainCtrl.js
+++ LittleHero/www/main/MainCtrl.js
@@ -6,81 +6,146 @@ angular.module('littleHero').controller('MainController', function($scope, $stat
 
   $scope.equipment = [
     {
+      type: 'animal',
+      iconSrc: 'img/animal1_icon.svg',
+      imgSrc: 'img/animal1.svg'
+    },
+    {
       type: 'eyes',
-      iconSrc: 'img/eyes1-icon.svg',
+      iconSrc: 'img/eyes1_icon.svg',
       imgSrc: 'img/eyes1.svg'
     },
     {
+      type: 'eyes',
+      iconSrc: 'img/eyes2_icon.svg',
+      imgSrc: 'img/eyes2.svg'
+    },
+    {
+      type: 'eyes',
+      iconSrc: 'img/eyes3_icon.svg',
+      imgSrc: 'img/eyes3.svg'
+    },
+    {
       type: 'hair',
-      iconSrc: 'img/hair1-icon.svg',
+      iconSrc: 'img/hair1_icon.svg',
       imgSrc: 'img/hair1.svg'
     },
     {
+      type: 'hat',
+      iconSrc: 'img/hat1_icon.svg',
+      imgSrc: 'img/hat1.svg'
+    },
+    {
+      type: 'hat',
+      iconSrc: 'img/hat2_icon.svg',
+      imgSrc: 'img/hat2.svg'
+    },
+    {
+      type: 'hat',
+      iconSrc: 'img/hat3_icon.svg',
+      imgSrc: 'img/hat3.svg'
+    },
+    {
       type: 'mouth',
-      iconSrc: 'img/mouth1-icon.svg',
+      iconSrc: 'img/mouth1_icon.svg',
       imgSrc: 'img/mouth1.svg'
     },
     {
+      type: 'mouth',
+      iconSrc: 'img/mouth2_icon.svg',
+      imgSrc: 'img/mouth2.svg'
+    },
+    {
+      type: 'mouth',
+      iconSrc: 'img/mouth3_icon.svg',
+      imgSrc: 'img/mouth3.svg'
+    },
+    {
       type: 'nose',
-      iconSrc: 'img/nose1-icon.svg',
+      iconSrc: 'img/nose1_icon.svg',
       imgSrc: 'img/nose1.svg'
     },
     {
+      type: 'nose',
+      iconSrc: 'img/nose2_icon.svg',
+      imgSrc: 'img/nose2.svg'
+    },
+    {
+      type: 'nose',
+      iconSrc: 'img/nose3_icon.svg',
+      imgSrc: 'img/nose3.svg'
+    },
+    {
       type: 'tee',
-      iconSrc: 'img/tee1-icon.svg',
+      iconSrc: 'img/tee1_icon.svg',
       imgSrc: 'img/tee1.svg',
     },
     {
       type: 'tee',
-      iconSrc: 'img/tee2-icon.svg',
+      iconSrc: 'img/tee2_icon.svg',
       imgSrc: 'img/tee2.svg'
     },
     {
       type: 'tee',
-      iconSrc: 'img/tee3-icon.svg',
+      iconSrc: 'img/tee3_icon.svg',
       imgSrc: 'img/tee3.svg'
     },
     {
       type: 'tee',
-      iconSrc: 'img/tee4-icon.svg',
+      iconSrc: 'img/tee4_icon.svg',
       imgSrc: 'img/tee4.svg'
     },
     {
       type: 'pants',
-      iconSrc: 'img/pants1-icon.svg',
+      iconSrc: 'img/pants1_icon.svg',
       imgSrc: 'img/pants1.svg'
     },
     {
       type: 'pants',
-      iconSrc: 'img/pants2-icon.svg',
+      iconSrc: 'img/pants2_icon.svg',
       imgSrc: 'img/pants2.svg'
+    },
+    {
+      type: 'shoes',
+      iconSrc: 'img/shoes1_icon.svg',
+      imgSrc: 'img/shoes1.svg'
+    },
+    {
+      type: 'shoes',
+      iconSrc: 'img/shoes2_icon.svg',
+      imgSrc: 'img/shoes2.svg'
+    },
+    {
+      type: 'shoes',
+      iconSrc: 'img/shoes3_icon.svg',
+      imgSrc: 'img/shoes3.svg'
     }
   ];
 
   $scope.buttons = [
     {
       type: 'all',
-      iconSrc: 'img/pants2-icon.svg'
+      iconSrc: 'img/pants2_icon.svg'
     },
     {
       type: 'all',
-      iconSrc: 'img/pants2-icon.svg'
+      iconSrc: 'img/pants2_icon.svg'
     },
     {
       type: 'tees',
-      iconSrc: 'img/pants2-icon.svg'
+      iconSrc: 'img/pants2_icon.svg'
     },
     {
       type: 'pants',
-      iconSrc: 'img/pants2-icon.svg'
+      iconSrc: 'img/pants2_icon.svg'
     },
     {
       type: 'weapons',
-      iconSrc: 'img/pants2-icon.svg'
+      iconSrc: 'img/pants2_icon.svg'
     },
     {
       type: 'pets',
-      iconSrc: 'img/pants2-icon.svg'
+      iconSrc: 'img/pants2_icon.svg'
     },
   ]
 
--- LittleHero/www/services/userService.js
+++ LittleHero/www/services/userService.js
@@ -1,17 +1,12 @@
 angular.module('littleHero').factory('user', function(){
-  //autoryzacja
-  //ściągnięcie z serwera(? za pierwszym razem)
-  //aktualizacja (spr czy połączenie z netem)
-  var data = {
+  return {
     get: function() {
       if (!promise) {
         var promise = $http.get('path/to/json.js')
           .success(function(response) {
             return response.data;
-          )};
+          });
       }
     }
   };
-  
-  return data;
 });
