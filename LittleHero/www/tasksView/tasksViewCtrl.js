e07edd0430e66db2e83205adadbe5447d17e934e
violetcat <zhalia14@gmail.com>
2016-06-15 21:58:32 +0200

    add swipe from tasks to tutor

--- LittleHero/www/tasksView/tasksViewCtrl.js
+++ LittleHero/www/tasksView/tasksViewCtrl.js
@@ -1,14 +1,18 @@
 angular.module('littleHero').controller('tasksViewController', function($scope, $state, $stateParams, dataService) {
 
-    $scope.$on('$ionicView.beforeEnter', function(){  
+    $scope.$on('$ionicView.beforeEnter', function(){
 
         $scope.user = $stateParams.user;
         $scope.currentAvatar = $stateParams.currentAvatar;
-   
+
         if ($scope.allAvatarTasks.length == 0 && $scope.currentAvatar != null) {
             $scope.getTasks();
         }
     });
+
+    $scope.swipeLeft = function() {
+        $state.go("mainTutor", { "allAvatars" : $scope.allAvatars, "currentAvatar" : $scope.currentAvatar, "user" : $scope.user });
+    };
 
     $scope.allAvatarTasks = []
 
@@ -39,9 +43,9 @@ angular.module('littleHero').controller('tasksViewController', function($scope, 
         dataService.patchTaskFullyCompleted($scope.currentAvatar["id"], patchContent).then(function(res) {
             console.log(res.data);
         });
-        
+
         $scope.deleteTask(task);
-    
+
     };
 
     $scope.deleteTask = function(task) {
