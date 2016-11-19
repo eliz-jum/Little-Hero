a3eb13f50466621555bdf9abdc7e69b57e699121
siulkilulki <jurkiewiczdawid@gmail.com>
2016-11-19 19:50:27 +0100

    attempt to host server on pythonanywhere

--- little_hero_rest_api/app.py
+++ little_hero_rest_api/app.py
@@ -1,6 +1,7 @@
 # Application bootstrap file
 # Logic for configuring and starting Flask app
 import logging.config
+from socket import gethostname
 
 from flask import Flask, Blueprint
 from little_hero_rest_api import settings
@@ -13,9 +14,8 @@ app = Flask(__name__)  # Create a Flask WSGI application
 logging.config.fileConfig('../logging.conf')
 log = logging.getLogger(__name__)
 
-
 def configure_app(flask_app):
-    flask_app.config['SERVER_NAME'] = settings.FLASK_SERVER_NAME
+    #flask_app.config['SERVER_NAME'] = settings.FLASK_SERVER_NAME
     flask_app.config['SQLALCHEMY_DATABASE_URI'] = settings.SQLALCHEMY_DATABASE_URI
     flask_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = settings.SQLALCHEMY_TRACK_MODIFICATIONS
     flask_app.config['SWAGGER_UI_DOC_EXPANSION'] = settings.RESTPLUS_SWAGGER_UI_DOC_EXPANSION
@@ -40,8 +40,10 @@ def initialize_app(flask_app):
 
 def main():
     initialize_app(app)
-    log.info('>>>>> Starting development server at http://{}/api/ <<<<<'.format(app.config['SERVER_NAME']))
-    app.run(debug=settings.FLASK_DEBUG)
+    #log.info('>>>>> Starting development server at http://{}/api/ <<<<<'.format(app.config['SERVER_NAME']))
+    if 'liveconsole' not in gethostname():
+        app.run(debug=settings.FLASK_DEBUG)
+
 
 if __name__ == "__main__":
     main()
