c91f6287adcb28dc7d6e4e6d60cec23f53a864fa
siulkilulki <jurkiewiczdawid@gmail.com>
2016-11-28 21:55:08 +0100

    for heroku

--- little_hero_rest_api/app.py
+++ little_hero_rest_api/app.py
@@ -18,7 +18,7 @@ logging.config.fileConfig('logging.conf')
 log = logging.getLogger(__name__)
 
 def configure_app(flask_app):
-    flask_app.config['SERVER_NAME'] = settings.FLASK_SERVER_NAME
+    #flask_app.config['SERVER_NAME'] = settings.FLASK_SERVER_NAME
     flask_app.config['SQLALCHEMY_DATABASE_URI'] = settings.SQLALCHEMY_DATABASE_URI
     flask_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = settings.SQLALCHEMY_TRACK_MODIFICATIONS
     flask_app.config['SWAGGER_UI_DOC_EXPANSION'] = settings.RESTPLUS_SWAGGER_UI_DOC_EXPANSION
@@ -47,9 +47,9 @@ def initialize_app(flask_app):
 
 def main():
     initialize_app(app)
-    log.info('>>>>> Starting development server at http://{}/api/ <<<<<'.format(app.config['SERVER_NAME']))
-    if 'liveconsole' not in gethostname():
-        app.run(debug=settings.FLASK_DEBUG)
+    #log.info('>>>>> Starting development server at http://{}/api/ <<<<<'.format(app.config['SERVER_NAME']))
+    #if 'liveconsole' not in gethostname():
+    app.run(debug=settings.FLASK_DEBUG)
 
 
 if __name__ == "__main__":
