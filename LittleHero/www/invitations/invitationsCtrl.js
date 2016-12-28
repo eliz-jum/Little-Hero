a8557a5ffa747e3fe718917bef5f0f74eaf69a29
Dosia <Dosia>
2016-12-28 21:51:07 +0100

    [invitations temp] add basic template + mock data

--- LittleHero/www/invitations/invitationsCtrl.js
+++ LittleHero/www/invitations/invitationsCtrl.js
@@ -1,2 +1,9 @@
 angular.module('littleHero').controller('InvitationsController', function($scope, $state, $stateParams, $ionicModal){
+  $scope.invites = [
+    "invite 1",
+    "invite 2",
+    "invite 3",
+    "invite 4",
+    "invite 5"
+  ];
 });
