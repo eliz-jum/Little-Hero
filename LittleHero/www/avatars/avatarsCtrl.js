9ba2f3c2a7e6be5524107db10d16f14b5a0d14b0
Dosia <Dosia>
2017-01-05 22:29:04 +0100

    [avatars] add modal

--- LittleHero/www/avatars/avatarsCtrl.js
+++ LittleHero/www/avatars/avatarsCtrl.js
@@ -1,9 +1,25 @@
-angular.module('littleHero').controller('AvatarsController', function($scope, $state, dataService, childService, ionicToast){
+angular.module('littleHero').controller('AvatarsController', function($scope, $state, dataService, childService, ionicToast, $ionicModal){
     $scope.user = childService.childObj;
     $scope.avatars = childService.avatarList;
+
+    $scope.avatars.forEach(function(item) {
+
+    });
     console.log(childService.avatarList);
 
     $scope.edit = function (avatar) {};
 
-    $scope.addAvatar = function () {};
+    $ionicModal.fromTemplateUrl('avatars/requestAvatar.html', {
+        scope: $scope
+    }).then(function (modal) {
+        $scope.modal = modal;
+    });
+
+    $scope.openModal = function () {
+        $scope.modal.show();
+    };
+
+    $scope.closeModal = function () {
+        $scope.modal.hide();
+    }
 });
