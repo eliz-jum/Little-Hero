3849058171c8972b9f3680e41ec25701f8589aae
Dosia <Dosia>
2017-01-06 01:15:47 +0100

    replace app.js with ionic branch version

--- LittleHero/www/js/app.js
+++ LittleHero/www/js/app.js
@@ -5,105 +5,108 @@
 // the 2nd parameter is an array of 'requires'
 angular.module('littleHero', ['ionic', 'ui.router', 'ionic-toast'])
 
-  .run(function ($ionicPlatform) {
-    $ionicPlatform.ready(function () {
-      if (window.cordova && window.cordova.plugins.Keyboard) {
-        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-        // for form inputs)
-        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    .run(function ($ionicPlatform) {
+        $ionicPlatform.ready(function () {
+            if (window.cordova && window.cordova.plugins.Keyboard) {
+                // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+                // for form inputs)
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
 
-        // Don't remove this line unless you know what you are doing. It stops the viewport
-        // from snapping when text inputs are focused. Ionic handles this internally for
-        // a much nicer keyboard experience.
-        cordova.plugins.Keyboard.disableScroll(true);
-      }
-      if (window.StatusBar) {
-        StatusBar.styleDefault();
-      }
+                // Don't remove this line unless you know what you are doing. It stops the viewport
+                // from snapping when text inputs are focused. Ionic handles this internally for
+                // a much nicer keyboard experience.
+                cordova.plugins.Keyboard.disableScroll(true);
+            }
+            if (window.StatusBar) {
+                StatusBar.styleDefault();
+            }
+        });
+    })
+
+    .config(function ($stateProvider, $urlRouterProvider) {
+        $stateProvider
+            .state('login', {
+                url: '/login',
+                templateUrl: 'login/login.html',
+                controller: 'LoginController',
+                params: {
+                    user: null
+                }
+            })
+            .state('main', {
+                url: '/main',
+                templateUrl: 'main/main.html',
+                controller: 'MainController',
+                params: {
+                    allAvatars: null,
+                    currentAvatar: null,
+                    user: null
+                }
+            })
+            .state('mainTutor', {
+                url: '/mainTutor',
+                templateUrl: 'mainTutor/mainTutor.html',
+                controller: 'MTController',
+                params: {
+                    user : null,
+                    currentAvatar: null
+                }
+            })
+            .state('recoverPassword', {
+                url: '/recoverPassword',
+                templateUrl: 'recoverPass/recoverPass.html',
+                controller: 'RecoverPassController'
+            })
+            .state('registration', {
+                url: '/registration',
+                templateUrl: 'registration/registration.html',
+                controller: 'RegistrationController',
+                params: {
+                    username: null
+                }
+            })
+            .state('tasks', {
+                url: '/tasks',
+                templateUrl: 'tasks/tasks.html',
+                controller: 'TasksController',
+                params: {
+                    allAvatars: null,
+                    currentAvatar : null,
+                    user: null
+                }
+            })
+            .state('settings', {
+                url: '/settings',
+                templateUrl: 'settings/settings.html',
+                controller: 'SettingsController'
+            })
+            .state('tasksCreator', {
+                url: '/tasksCreator',
+                templateUrl: 'tasksCreator/tasksCreator.html',
+                controller: 'TasksCreatorController',
+                params: {
+                    user: null,
+                    currentAvatar: null
+                }
+            })
+            .state('tasksView', {
+                url: '/tasksView',
+                templateUrl: 'tasksView/tasksView.html',
+                controller: 'tasksViewController',
+                params: {
+                    user: null,
+                    currentAvatar: null
+                }
+            })
+            .state('notifications',{
+                url: '/notifications',
+                templateUrl: 'notifications/notifications.html',
+                controller: 'NotificationsController',
+                params: {
+                    allAvatars: null,
+                    currentAvatar : null,
+                    user: null
+                }
+            }),
+            $urlRouterProvider.otherwise('/login');
     });
-  })
-
-  .config(function ($stateProvider, $urlRouterProvider) {
-    $stateProvider
-      .state('login', {
-        url: '/login',
-        templateUrl: 'login/login.html',
-        controller: 'LoginController',
-        params: {
-          user: null
-        }
-      })
-      .state('main', {
-        url: '/main',
-        templateUrl: 'main/main.html',
-        controller: 'MainController',
-        params: {
-          allAvatars: null,
-          currentAvatar: null,
-          user: null
-        }
-      })
-      .state('mainTutor', {
-        url: '/mainTutor',
-        templateUrl: 'mainTutor/mainTutor.html',
-        controller: 'MTController',
-        params: {
-          user: null,
-          currentAvatar: null
-        }
-      })
-      .state('recoverPassword', {
-        url: '/recoverPassword',
-        templateUrl: 'recoverPass/recoverPass.html',
-        controller: 'RecoverPassController'
-      })
-      .state('registration', {
-        url: '/registration',
-        templateUrl: 'registration/registration.html',
-        controller: 'RegistrationController',
-        params: {
-          username: null
-        }
-      })
-      .state('tasks', {
-        url: '/tasks',
-        templateUrl: 'tasks/tasks.html',
-        controller: 'TasksController',
-        params: {
-          allAvatars: null,
-          currentAvatar: null,
-          user: null
-        }
-      })
-      .state('settings', {
-        url: '/settings',
-        templateUrl: 'settings/settings.html',
-        controller: 'SettingsController'
-      })
-      .state('taskCreator', {
-        url: '/taskCreator',
-        templateUrl: 'taskCreator/taskCreator.html',
-        controller: 'TaskCreatorController',
-        params: {
-          user: null,
-          currentAvatar: null
-        }
-      })
-      .state('notifications', {
-        url: '/notifications',
-        templateUrl: 'notifications/notifications.html',
-        controller: 'NotificationsController',
-        params: {
-          allAvatars: null,
-          currentAvatar: null,
-          user: null
-        }
-
-      })
-      .state('avatars', {
-        url: '/avatars',
-        templateUrl: 'avatars/avatars.html',
-        controller: 'AvatarsController',
-        params: {}
-      }),$urlRouterProvider.otherwise('/login');
-  });
