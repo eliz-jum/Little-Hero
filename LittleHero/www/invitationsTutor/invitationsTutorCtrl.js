cc2b94376c0f4bde9d14f97fa01b8100b342bc5b
Zofia Drozd <zegarynka@wp.pl>
2017-01-09 21:03:21 +0100

    [invites tutor] add new invite to the list

--- LittleHero/www/invitationsTutor/invitationsTutorCtrl.js
+++ LittleHero/www/invitationsTutor/invitationsTutorCtrl.js
@@ -84,9 +84,11 @@ angular.module('littleHero').controller('InvitationsTutorController', function (
     };
 
     $scope.sendInvite = function (child) {
-        dataService.postInvites('tutors', childService.tutorObj.id, {child_id: child.id, kind: "tutor"});
-        $scope.closeModal();
-        $scope.showToast("Zaproszenie wysłane");
+        dataService.postInvites('tutors', childService.tutorObj.id, {child_id: child.id, kind: "tutor"}).then(function(res) {
+            $scope.invites.push(res);
+            $scope.closeModal();
+            $scope.showToast("Zaproszenie wysłane");
+        });
     };
 
     $scope.showToast = function(message){
