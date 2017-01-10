47d558aefca874a335f59076bf0c938929bbf412
violetcat <zhalia14@gmail.com>
2017-01-10 23:00:23 +0100

    function hardcodeAvatarArrays works on currentAvatar level

--- LittleHero/www/services/childService.js
+++ LittleHero/www/services/childService.js
@@ -1700,7 +1700,7 @@ angular.module('littleHero').factory('childService',function($state, dataService
       if (item.price == 0)
         childService.canBePutOnItems.push(item);
       else if (item.clazz == childService.currentAvatar.clazz || item.clazz == "allClasses"){
-        if (item.level == 1)
+        if (item.level == childService.currentAvatar.level)
           childService.canBePurchasedItems.push(item);
         else
           childService.unavailableItems.push(item);
