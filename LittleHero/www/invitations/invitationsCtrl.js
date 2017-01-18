95ef2b4ea49f85a9656bf99a8e1b0d932fde9710
Zofia Drozd <zegarynka@wp.pl>
2017-01-18 21:59:35 +0100

    send user to main after spinner closes

--- LittleHero/www/invitations/invitationsCtrl.js
+++ LittleHero/www/invitations/invitationsCtrl.js
@@ -63,16 +63,15 @@ angular.module('littleHero').controller('InvitationsController', function ($scop
         $scope.closeModal("newAvatar");
       $scope.showSpinner = true;
       console.log("invite", $scope.invite);
-      childService.addNewAvatar($scope.newAvatar.name, $scope.newAvatar.class, $scope.tutorId);
-    $scope.closeModal("newAvatar");
 
     // dataService.deleteInvite('children', childService.childObj.id, $scope.invite.id).then(function () {
     //       var index = $scope.invites.indexOf($scope.invite);
     //       $scope.invites.splice(index, 1);
-            setTimeout(function() {
-              $scope.showSpinner = false;
-            },4000);
-    //       $scope.closeModal("newAvatar");
+    //       $scope.closeModal("newAvatar")
+    setTimeout(function() {
+      $scope.showSpinner = false;
+      $state.go('main');
+    },5000);
     //       $scope.showToast("Utworzono nowego awatara!");
     //   });
       }
