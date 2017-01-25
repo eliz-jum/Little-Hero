81d372c513ef313a0eb9ec02748440d728e890ab
siulkilulki <jurkiewiczdawid@gmail.com>
2017-01-25 21:29:07 +0100

    add interceptor security code

--- LittleHero/www/services/interceptorService.js
+++ LittleHero/www/services/interceptorService.js
@@ -1,12 +1,23 @@
+var secretKey = 'dev'
+
 angular.module('littleHero').factory('httpRequestInterceptor', function () {
-    return {
-      request: function (config) {
-        config.headers['Authorization'] = 'Basic d2VudHdvcnRobWFuOkNoYW5nZV9tZQ==';
-        return config;
+  return {
+    request: function (config) {
+      if (config.url.includes("http")) {
+        var url = config.url;
+        var method = config.method;
+        var timestamp = Date.now().toString();
+        var nonce = Math.random().toString();
+        var message = method + '+' + url + '+' + timestamp + '+' + nonce
+        var signature = CryptoJS.HmacSHA1(message, secretKey).toString(CryptoJS.enc.Base64);
+        config.headers['Authorization'] = 'hmac ' + timestamp + ':' + nonce + ':' + signature;
+        console.log(method + '\n' + url + '\n' + config.headers['Authorization'])
       }
-    };
+      return config;
+    }
+  };
 });
 
 angular.module('littleHero').config(function ($httpProvider) {
-    $httpProvider.interceptors.push('httpRequestInterceptor');
+  $httpProvider.interceptors.push('httpRequestInterceptor');
 });
