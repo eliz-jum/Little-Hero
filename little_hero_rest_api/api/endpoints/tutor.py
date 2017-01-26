7b12dcd073b57a40db4f1ebf0f4708efb3f7f72f
siulkilulki <jurkiewiczdawid@gmail.com>
2017-01-26 12:21:38 +0100

    prettify space

--- little_hero_rest_api/api/endpoints/tutor.py
+++ little_hero_rest_api/api/endpoints/tutor.py
@@ -50,7 +50,7 @@ class TutorsCollection(Resource):
         tutor = tutor_dao.create(data)
         msg = Message('Welcome to Little Hero ' + tutor.login + '!',
                       sender='registration@mylittlehero.eu', recipients=[tutor.mail])
-        msg.body = 'Welcome to Little Hero! \n\n    You have successfully registered as a tutor. \n ' \
+        msg.body = 'Welcome to Little Hero! \n\n    You have successfully registered as a tutor. \n' \
                    '    Your login is: ' + tutor.login + '.'
         mail.send(msg)
 
